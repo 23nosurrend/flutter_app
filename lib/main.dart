840bc9d952279c49d73fe15e0e4817a8c7a6ee77
23nosurrend <keynesbizimana@gmail.com>
2024-08-21 14:37:37 +0200

    ft (quotes  screen):added Quotes screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,57 @@
 import 'package:flutter/material.dart';
+import "quote.dart";
 
 void main() {
-  runApp(MaterialApp(home: UserCard()));
+  runApp(MaterialApp(
+    home: QuotesLists(),
+  ));
 }
 
-class UserCard extends StatefulWidget {
+class QuotesLists extends StatefulWidget {
   @override
-  State<UserCard> createState() => _UserCardState();
+  State<QuotesLists> createState() => _QuotesState();
 }
 
-class _UserCardState extends State<UserCard> {
-  int level = 0;
+class _QuotesState extends State<QuotesLists> {
+  List<Quote> quotes = [
+    Quote(
+        text: "The greatest war are person , embrace them with passion",
+        author: "K biz"),
+    Quote(
+        text: "Proving that it is your passion does not work like magic ",
+        author: "K biz")
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin:EdgeInsets.fromLTRB(16, 16, 16,0),
+      child:Column(
+        children: [
+          Text(
+            quote.text,
+            style:TextStyle(
+              fontSize: 20,
+              color:Colors.grey
+            )
+          )
+        ],
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black87,
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("User Card"),
+        title: Text("Awesome Quotes"),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level += 1;
-          });
-        },
-        backgroundColor: Colors.grey,
-        child: Icon(Icons.add),
-      ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/image3.jpg'),
-                radius: 50,
-              ),
-            ),
-            Divider(
-              height: 50.0,
-              color: Colors.blue,
-            ),
-            Text(
-              "Name",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "Keynes Bizimana",
-              style: TextStyle(
-                  color: Colors.blue,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              "Current Developer Level",
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              "$level",
-              style: TextStyle(
-                fontSize: 40,
-                  color: Colors.blue,
-                  letterSpacing: 2.0,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: [
-                Icon(Icons.email_outlined, color: Colors.grey),
-                SizedBox(width: 10.0),
-                Text(
-                  "keynesbizimana@gmail.com",
-                  style: TextStyle(
-                      color: Colors.grey, fontSize: 18, letterSpacing: 1.8),
-                ),
-               
-               
-              ],
-            ),
-             Center(
-               child: ElevatedButton(
-                    onPressed: (){},
-                    
-                     style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Colors.blue)
-                     ),
-                      child: Text("Submit"),
-                     ),
-             )
-          ],
-        ),
-      ),
+      body: Column(
+          children: quotes
+              .map((quote) => Text('${quote.text} - ${quote.author}'))
+              .toList()),
     );
   }
 }
